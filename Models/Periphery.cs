e21ea9774f22ddf237559d9f9f2afbbe94ec3bd7
Anton Stepanyuck <anton.stepanyuk@atos.net>
2022-07-27 18:32:40 +0300

    Закомментирована сущность "Переферия"

--- Models/Periphery.cs
+++ Models/Periphery.cs
@@ -5,21 +5,27 @@ namespace FramingWorkshop.Models
     // Сущность "Переферия" (стекло,лепестки,скобы)
     internal class Periphery : ViewModel
     {
-        private string title;
-        private double price;
 
         public int Id { get; set; }
 
+        #region Названиеи переферии
+        /// <summary> Названиеи переферии </summary>
+        private string title;
         public string Title
         {
             get { return title; }
             set => Set(ref title, value);
         }
+        #endregion
 
+        #region Цена переферии
+        /// <summary> Цена переферии </summary>
+        private double price;
         public double Price
         {
             get { return price; }
             set => Set(ref price, value);            
         }       
+        #endregion
     }
 }

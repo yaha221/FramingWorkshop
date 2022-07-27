fcb7bd5c98f156000df83e14ec809d030d772a73
Anton Stepanyuck <anton.stepanyuk@atos.net>
2022-07-27 18:21:28 +0300

    Добавлено новое окно для добавления багета

---
+++ View/AddFrameWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using FramingWorkshop.Controller;
+using System.Windows;
+
+namespace FramingWorkshop.View
+{
+    /// <summary>
+    /// Логика взаимодействия для AddFrameWindow.xaml
+    /// </summary>
+    public partial class AddFrameWindow : Window
+    {
+        internal Frame Frame { get; private set; }
+        internal AddFrameWindow(Frame f)
+        {
+            InitializeComponent();
+            Frame = f;
+            DataContext = new AddFrameVM();
+        }
+    }
+}
